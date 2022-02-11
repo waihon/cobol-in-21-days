37ecf00130dbb80dcfedd316b25afa6d3abe1a49
Waihon Yew <yewwaihon@gmail.com>
2022-02-11 23:03:08 +0800

    Add another constant

---
+++ 02-variables-and-constants/helohelo.cbl
@@ -0,0 +1,15 @@
+      * Adding another constant.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELOHELO.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+
+       PROGRAM-BEGIN.
+           DISPLAY "Hello world".
+           DISPLAY "I said, Hello world".
+
+       PROGRAM-DONE.
+           STOP RUN.
+
+        

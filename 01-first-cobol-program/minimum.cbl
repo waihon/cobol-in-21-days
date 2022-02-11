d259c0da87311c51846812552bb23adc1d98bd06
Waihon Yew <yewwaihon@gmail.com>
2022-02-11 22:54:16 +0800

    The irreducible minimum COBOL program

---
+++ 01-first-cobol-program/minimum.cbl
@@ -0,0 +1,11 @@
+      * The irreducible minimum COBOL program.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. MINIMUM.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+
+       PROGRAM-DONE.
+           STOP RUN.
+
+

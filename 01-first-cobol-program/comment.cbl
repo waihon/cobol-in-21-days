7e0fa62a115be45c522287aa272c127f13de1582
Waihon Yew <yewwaihon@gmail.com>
2022-02-11 22:55:37 +0800

    The areas of a COBOL program

---
+++ 01-first-cobol-program/comment.cbl
@@ -0,0 +1,15 @@
+000050* The areas of a COBOL program.
+000100 IDENTIFICATION DIVISION.
+000200 PROGRAM-ID. COMMENT.
+000300 ENVIRONMENT DIVISION.
+       DATA DIVISION.                                                   MB072197
+000500 PROCEDURE DIVISION.
+000600
+000700* This is a comment.                                              MB072197
+000800* This paragraph displays information about the program.          MB072197
+000900 PROGRAM-BEGIN.
+003700     DISPLAY "This program contains four DIVISIONS,".             MB072197
+003800     DISPLAY "three PARAGRAPHS".                                  MB072197
+001000     DISPLAY "and four SENTENCES.".
+001100 PROGRAM-DONE.
+001200     STOP-RUN.

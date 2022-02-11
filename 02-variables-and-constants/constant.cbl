de49a1ba088b650f2222a097e5c18c656a95d473
Waihon Yew <yewwaihon@gmail.com>
2022-02-11 23:03:26 +0800

    String and numeric constants

---
+++ 02-variables-and-constants/constant.cbl
@@ -0,0 +1,16 @@
+      * String and numeric constants.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. CONSTANT.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+
+       PROGRAM-BEGIN.
+           DISPLAY "Hello world".
+           DISPLAY 55.
+           DISPLAY 12.607.
+
+       PROGRAM-DONE.
+           STOP RUN.
+
+        

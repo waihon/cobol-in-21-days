45f4872e40303b1f3c387e495690da556d07c405
Waihon Yew <yewwaihon@gmail.com>
2022-02-11 22:54:59 +0800

    Three levels of COBOL grammar

---
+++ 01-first-cobol-program/sentences.cbl
@@ -0,0 +1,14 @@
+      * Three levels of COBOL grammar: division, paragraph, sentence.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. SENTENCES.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+
+       PROGRAM-BEGIN.
+           DISPLAY "This program contain four DIVISIONS,".
+           DISPLAY "three PARAGRAPHS,".
+           DISPLAY "and four SENTENCES.".
+       PROGRAM-DONE.
+           STOP RUN.
+          

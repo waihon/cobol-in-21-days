53f0d679c40a8234bcd00802034feb4d13356e0c
Waihon Yew <yewwaihon@gmail.com>
2022-02-11 23:02:49 +0800

    Modify a constant by editing and recompiling

---
+++ 02-variables-and-constants/hello.cbl
@@ -0,0 +1,14 @@
+      * Modifying a constant by editing and recompiling.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+
+       PROGRAM-BEGIN.
+           DISPLAY "I said, Hello world".
+
+       PROGRAM-DONE.
+           STOP RUN.
+
+        

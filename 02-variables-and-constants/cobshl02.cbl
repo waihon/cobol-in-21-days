e26daaf9a7c7e1c50c82cf8e834a299a5c701bb3
Waihon Yew <yewwaihon@gmail.com>
2022-02-12 00:04:57 +0800

    Revised version of a COBOL shell

---
+++ 02-variables-and-constants/cobshl02.cbl
@@ -0,0 +1,17 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. COBSHL02.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+
+       WORKING-STORAGE SECTION.
+
+       01  XXX               PIC X(20).
+       01  YYY               PIC 9(03).
+    
+       PROCEDURE DIVISION.
+
+       PROGRAM-BEGIN.
+
+       PROGRAM-DONE.
+           STOP RUN.
+          

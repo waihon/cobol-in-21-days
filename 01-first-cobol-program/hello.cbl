b039a321e0758b9bea17858f10e4fb2e604e5944
Waihon Yew <yewwaihon@gmail.com>
2022-02-11 22:52:03 +0800

    Hello World in COBOL

---
+++ 01-first-cobol-program/hello.cbl
@@ -0,0 +1,14 @@
+      * "Hello World" in COBOL.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+
+       PROGRAM-BEGIN.
+           DISPLAY "Hello World".
+
+       PROGRAM-DONE.
+           STOP RUN.
+
+        

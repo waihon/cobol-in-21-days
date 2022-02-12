05c3a9afddf9fcbaddbd6bcc200d91a81d6563e8
Waihon Yew <yewwaihon@gmail.com>
2022-02-12 18:41:50 +0800

    Use shorter version of comparison operator

---
+++ 04-decision-making/yesno04.cbl
@@ -0,0 +1,54 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. YESNO04.
+      *--------------------------------------------------
+      * This program asks for a Y or N answer, and then
+      * displays whether the user chose yes or no.
+      * The edit logic allows for entry of Y, y, N, or n.
+      *--------------------------------------------------
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+
+       WORKING-STORAGE SECTION.
+
+       01  YES-OR-NO         PIC X.
+    
+       PROCEDURE DIVISION.
+
+       PROGRAM-BEGIN.
+
+           PERFORM GET-THE-ANSWER.
+
+           PERFORM EDIT-THE-ANSWER.
+
+           PERFORM DISPLAY-THE-ANSWER.
+
+       PROGRAM-DONE.
+           STOP RUN.
+
+       GET-THE-ANSWER.
+      
+           DISPLAY "Is the answer Yes or No? (Y/N)".
+           ACCEPT YES-OR-NO.
+
+       EDIT-THE-ANSWER.
+
+           IF YES-OR-NO = "y"
+              MOVE "Y" TO YES-OR-NO.
+
+           IF YES-OR-NO = "n"
+              MOVE "N" TO YES-OR-NO.              
+
+       DISPLAY-THE-ANSWER.
+
+           IF YES-OR-NO = "Y"
+              PERFORM IT-IS-VALID
+              DISPLAY "You answered Yes.".
+
+           IF YES-OR-NO = "N"
+              PERFORM IT-IS-VALID
+              DISPLAY "You answered No.".
+
+       IT-IS-VALID.
+           
+           DISPLAY "Your answer is valid and".
+          

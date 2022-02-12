3a9b36e383bb169d4219fb8b511d652d5021eabc
Waihon Yew <yewwaihon@gmail.com>
2022-02-12 18:13:12 +0800

    Edit the answer by transforming y and n to Y and N

---
+++ 04-decision-making/yesno02.cbl
@@ -0,0 +1,48 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. YESNO02.
+      *--------------------------------------------------
+      * This program asks for a Y or N answer, and then
+      * displays whether the user chose yes or no.
+      * The edit logic allows for entry of Y, y, N, or n.
+      *--------------------------------------------------
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+
+       WORKING-STORAGE SECTION.
+
+       01  YES-OR-NO         PIC X.
+    
+       PROCEDURE DIVISION.
+
+       PROGRAM-BEGIN.
+
+           PERFORM GET-THE-ANSWER.
+
+           PERFORM EDIT-THE-ANSWER.
+
+           PERFORM DISPLAY-THE-ANSWER.
+
+       PROGRAM-DONE.
+           STOP RUN.
+
+       GET-THE-ANSWER.
+      
+           DISPLAY "Is the answer Yes or No? (Y/N)".
+           ACCEPT YES-OR-NO.
+
+       EDIT-THE-ANSWER.
+
+           IF YES-OR-NO IS EQUAL "y"
+              MOVE "Y" TO YES-OR-NO.
+
+           IF YES-OR-NO IS EQUAL "n"
+              MOVE "N" TO YES-OR-NO.              
+
+       DISPLAY-THE-ANSWER.
+
+           IF YES-OR-NO IS EQUAL "Y"
+              DISPLAY "You answered Yes.".
+
+           IF YES-OR-NO IS EQUAL "N"
+              DISPLAY "You answered No.".
+          

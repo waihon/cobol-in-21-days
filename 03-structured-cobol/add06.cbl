4bf462e7b377f3dfdc6194f5a8eda8163ae6ec59
Waihon Yew <yewwaihon@gmail.com>
2022-02-12 14:40:09 +0800

    Add a forced STOP RUN

---
+++ 03-structured-cobol/add06.cbl
@@ -0,0 +1,37 @@
+      * A forced STOP RUN.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. ADD06.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+
+       WORKING-STORAGE SECTION.
+
+       01  FIRST-NUMBER      PIC 99.
+       01  SECOND-NUMBER     PIC 99.
+       01  THE-RESULT        PIC 999.
+
+       PROCEDURE DIVISION.
+       
+       PROGRAM-BEGIN.
+
+           DISPLAY "This program will add 2 numbers.".
+
+           DISPLAY "Enter the first number.".
+
+           ACCEPT FIRST-NUMBER.
+
+           DISPLAY "Fooled you.".
+
+           STOP RUN.
+
+           DISPLAY "Enter the second number.".
+
+           ACCEPT SECOND-NUMBER.
+
+           COMPUTE THE-RESULT = FIRST-NUMBER + SECOND-NUMBER.
+
+           DISPLAY "The result is " THE-RESULT.
+
+       PROGRAM-DONE.
+           STOP RUN.
+          

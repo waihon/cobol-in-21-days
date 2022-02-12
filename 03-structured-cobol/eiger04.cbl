72f0a2b5eb1c28f19e4efc6cf59965a89ba3295d
Waihon Yew <yewwaihon@gmail.com>
2022-02-12 16:54:38 +0800

    PERFORM a paragraph for handling repetitive actions

---
+++ 03-structured-cobol/eiger04.cbl
@@ -0,0 +1,51 @@
+      * Using a PERFORMed paragraph.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. EIGER04.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+
+       WORKING-STORAGE SECTION.
+
+       01  THE-MESSAGE              PIC X(50).
+       01  THE-NUMBER               PIC 9(02).
+       01  A-SPACE                  PIC X(01).
+    
+       PROCEDURE DIVISION.
+       PROGRAM-BEGIN.
+
+      * Initialize the space variable
+           MOVE " " TO A-SPACE.
+      * Start THE-NUMBER at 0
+           MOVE 0 TO THE-NUMBER.
+
+      * Set up and display line 1
+           MOVE "There once was a lady from Eiger," TO THE-MESSAGE.
+           PERFORM ADD-NUMBER-AND-DISPLAY.
+
+      * Set up and display line 2
+           MOVE "Who smiled and rode forth on a tiger." TO THE-MESSAGE.
+           PERFORM ADD-NUMBER-AND-DISPLAY.
+
+      * Set up and display line 3
+           MOVE "They returned from the rider" TO THE-MESSAGE.
+           PERFORM ADD-NUMBER-AND-DISPLAY.
+
+      * Set up and display line 4
+           MOVE "With the lady inside," TO THE-MESSAGE.
+           PERFORM ADD-NUMBER-AND-DISPLAY.
+
+      * Set up and display line 5
+           MOVE "And the smile on the face of the tiger."
+              TO THE-MESSAGE.
+           PERFORM ADD-NUMBER-AND-DISPLAY.
+
+       PROGRAM-DONE.
+           STOP RUN.
+
+       ADD-NUMBER-AND-DISPLAY.
+           ADD 1 TO THE-NUMBER.
+           DISPLAY
+              THE-NUMBER
+              A-SPACE
+              THE-MESSAGE.
+          
